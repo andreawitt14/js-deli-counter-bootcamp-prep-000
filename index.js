602b4052ae64d9f47dc5f7b2394e138efa962b05
Andrea Witt <andreawitt14@hotmail.com>
2018-05-29 15:19:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, Name){
  katzDeliLine.push(Name);
  var numberInLine = katzDeliLine.length + 1;
- return "Welcome, '${Name}'"
+ return "Welcome, '${Name}'. You are number '${numberInLine}' in line.";
   
 }
+
