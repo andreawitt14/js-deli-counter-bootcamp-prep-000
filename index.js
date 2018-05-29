2310ea8c9100e7b3d9a9632d6c036bc85f11dd22
Andrea Witt <andreawitt14@hotmail.com>
2018-05-29 15:28:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ function takeANumber(katzDeliLine, Name){
  katzDeliLine.push(Name);
  var numberInLine = katzDeliLine.length;
  return "Welcome, " + Name + ". You are number " + numberInLine + " in line.";
-  
 }
 
