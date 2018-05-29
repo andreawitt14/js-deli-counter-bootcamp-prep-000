dde60bbe7bbe29429b6524e859f993ece5b54c78
Andrea Witt <andreawitt14@hotmail.com>
2018-05-29 15:59:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentline(katzDeliLine){
   else{
     var numbersAndNames = [];
     for(let i = 0; i < katzDeliLine.length; i++){
-      numbersAndNames[i] = lineNamesandNumbers.push(i+1 + ". "+ line[i])
+      numbersAndNames.push(i+1 + ". "+ line[i])
     }
   }
 }
